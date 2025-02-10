f0075d1fd8c1750bb1d97d1e10439605f9f86eb3
Facumasa <facundomasari@gmail.com>
2025-02-10 18:07:43 +0100

    My solution with forEach instead of for

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(catalog) {
+    let titles =[];
+    let i=0;
+    catalog.forEach(element => {
+        titles[i]=element.title;
+        i++;
+    });
+    return titles;
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
