102fcc60bed4347036c72f0ad1d6007684294d2e
Facumasa <facundomasari@gmail.com>
2025-02-10 11:08:13 +0100

    Better version with include and all the letters and numbers

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,26 +1,38 @@
 const palindromes = function (string) {
     string = string.toLowerCase();
     let array = [];
+    let array2 = [];
+    let i=0;
     // make the string an array:
     array = string.split("");
-    // if there are any spaces in the array:
-    if (array.includes(" ")) {
-        string = string.split(" ").join("");
-    }
-    if (array.includes(".")) {
-        string = string.split(".").join("");
-    }
-    if (array.includes(",")) {
-        string = string.split(",").join("");
-    }
-    if (array.includes("!")) {
-        string = string.split("!").join("");
-    }
-    if (array.includes("?")) {
-        string = string.split("?").join("");
-    }
-    // console.log("splitted string ",string);
-    // console.log("joined string ",string);
+    
+    // filters all non alphanumerical characters with many ifs
+    // if (array.includes(" ")) {
+    //     string = string.split(" ").join("");
+    // }
+    // if (array.includes(".")) {
+    //     string = string.split(".").join("");
+    // }
+    // if (array.includes(",")) {
+    //     string = string.split(",").join("");
+    // }
+    // if (array.includes("!")) {
+    //     string = string.split("!").join("");
+    // }
+    // if (array.includes("?")) {
+    //     string = string.split("?").join("");
+    // }
+
+    //another way to do it is with include
+    let alphanumerical="abcdesfhijklmnopqrstuvwxyz0123456789"
+    alphanumerical = alphanumerical.split("");
+    array.forEach(element => {
+        if (alphanumerical.includes(element)) {
+           array2[i]=element; 
+           i++;
+        }    
+    });
+    string = array2.join("");
     let reversed = string.split("").reverse().join("");
     if (string===reversed) {
         return true;
@@ -30,6 +42,6 @@ const palindromes = function (string) {
     }
 };
 
-palindromes("racecar");
+palindromes("r acecar");
 // Do not edit below this line
 module.exports = palindromes;
