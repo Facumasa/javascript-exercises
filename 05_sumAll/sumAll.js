4a408d7ac7607d3f768d43d1604d1d1b2b79bcb5
Facumasa <facundomasari@gmail.com>
2025-02-10 09:49:06 +0100

    Check for isInteger and typeof to see if is number and integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let sum = 0;
+    if (num1<0 || num2<0 || 
+        !Number.isInteger(num1) || !Number.isInteger(num2) ||
+        typeof num1 != "number" || typeof num2 != "number"
+        )  {
+        return "ERROR";
+    }
+    else {
+        if (num1<=num2) {
+            for (let index = num1; index <= num2; index++) {
+                sum += index;
+            }
+        }
+        else {
+            for (let index = num1; index >= num2; index--) {
+                sum += index;
+            }
 
+        }
+    }
+ return sum;
 };
 
 // Do not edit below this line
