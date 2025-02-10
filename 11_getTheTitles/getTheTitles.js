1b740aeb53f22143941b604ef29355f6b62a58a2
Facumasa <facundomasari@gmail.com>
2025-02-10 18:29:52 +0100

    Quite fun and complete, should check in the future

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -9,6 +9,18 @@ const getTheTitles = function(catalog) {
 };
 
 
+// The map() method of Array instances creates a new 
+// array populated with the results of calling a 
+// provided function on every element in the calling 
+// array.
+// 
+// 
+// The solution is:
+// 
+// const getTheTitles = function (array) {
+//     return array.map((book) => book.title);
+//   };
+  
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,47 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleDeath) {
+    let age = [];
+    let i = 0;
+    let oldestAge = 0;
+    peopleDeath.forEach(element => {
+        if (typeof element.yearOfDeath==='undefined') {
+            element.yearOfDeath=2025;
+        }
+        age[i]=element.yearOfDeath-element.yearOfBirth
+        i++;
+        console.log(age[i-1]);
+    });
 
+    // The Math.max() static method returns the largest 
+    // of the numbers given as input parameters, or 
+    // -Infinity if there are no parameters.
+    // ...age because we dont know how many numbers inside the array age
+    oldestAge = Math.max(...age);
+    console.log(oldestAge);
+    // indexOf says in which position of age is the number oldestAge
+    // since that is the position of the oldest person, we should return
+    // the data from that position of the given array
+    return peopleDeath[age.indexOf(oldestAge)];
 };
 
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
