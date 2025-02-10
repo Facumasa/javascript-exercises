fdb30a57223256672524fd4b447aad725cdadcd4
Facumasa <facundomasari@gmail.com>
2025-02-10 17:53:40 +0100

    Quite easy, maybe not the best way

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num>0) {
+        let sum=1;
+        let fibo = [0, 1];
+        for (let index = 2; index <= num; index++) {
+            fibo[index]=fibo[index-1]+fibo[index-2];
+        }
+        return fibo[num]
+    }
+    else {
+        return "OOPS"
+    }
 };
 
+console.log(fibonacci("6"));
 // Do not edit below this line
 module.exports = fibonacci;
