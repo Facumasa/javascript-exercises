bd9f5bdd7c43fd1efdcc1b115e13a5e7691529d9
Facumasa <facundomasari@gmail.com>
2025-02-10 10:29:52 +0100

    Too easy, nothing useful learned

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  let sum=num1+num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	let sub=num1-num2;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum=0;
+  array.forEach(element => {
+    sum += element;
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	let res=1;
+  array.forEach(element => {
+    res = res*element;
+  })
+  return res;
 };
 
-const power = function() {
-	
+
+
+const power = function(num1,num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let res = 1;
+  for (let index = 1; index <= num1; index++) {
+    res = res * index;
+  }
+  return res;
 };
+
+console.log(factorial(0));
 
 // Do not edit below this line
 module.exports = {
