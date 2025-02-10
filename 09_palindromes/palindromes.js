16fbc17a38bab8925b190b0cd3c79b8a0eede86d
Facumasa <facundomasari@gmail.com>
2025-02-10 10:57:01 +0100

    Works but too many ifs for every punctuation sign

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,35 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let array = [];
+    // make the string an array:
+    array = string.split("");
+    // if there are any spaces in the array:
+    if (array.includes(" ")) {
+        string = string.split(" ").join("");
+    }
+    if (array.includes(".")) {
+        string = string.split(".").join("");
+    }
+    if (array.includes(",")) {
+        string = string.split(",").join("");
+    }
+    if (array.includes("!")) {
+        string = string.split("!").join("");
+    }
+    if (array.includes("?")) {
+        string = string.split("?").join("");
+    }
+    // console.log("splitted string ",string);
+    // console.log("joined string ",string);
+    let reversed = string.split("").reverse().join("");
+    if (string===reversed) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
+palindromes("racecar");
 // Do not edit below this line
 module.exports = palindromes;
