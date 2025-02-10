f8f5302ac210317e26a86b6ed1fa5737c87c3d18
Facumasa <facundomasari@gmail.com>
2025-02-10 10:13:01 +0100

    Too easy doesnt worth it checking again

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+// The Math.round() static method returns the value of a
+// number rounded to the nearest integer.
+// console.log(Math.round(5.95), Math.round(5.5), Math.round(5.05));
+// Expected output: 6 6 5
+
+
+const convertToCelsius = function(tempInFa) {
+  temp = (tempInFa - 32)*(5/9);
+  temp = Math.round(temp*10)/10;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCel) {
+
+  temp = tempInCel * (9/5) + 32;
+  temp = Math.round(temp*10)/10;
+  return temp;
 };
 
 // Do not edit below this line
